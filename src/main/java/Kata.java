1920ee3d44911569c3561432fb79be3aee1a373a
nicolas.romanzin <nicolas.romanzin@thalesgroup.com>
2024-01-11 17:11:47 +0100

    1st acceptance criteria

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -2,4 +2,8 @@ public class Kata {
     public static void main(String[] args) {
         System.out.println("Welcome to the Dojo");
     }
+
+    public static boolean isLeapYear(int input) {
+        return input % 400 == 0;
+    }
 }
